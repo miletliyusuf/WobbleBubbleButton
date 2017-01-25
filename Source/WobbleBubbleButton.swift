0dde87d6d26e05e631092f9b321340849d093635
Yusuf <yusuf@eftsoftware.com>
2017-01-25 16:06:19 +0300

    Parallax amount increased

--- Source/WobbleBubbleButton.swift
+++ Source/WobbleBubbleButton.swift
@@ -53,7 +53,7 @@ open class WobbleBubbleButton: UIButton {
     }
     
     fileprivate func addParallaxToView(_ vw: UIView) {
-        let amount = 100
+        let amount = 20
         
         let horizontal = UIInterpolatingMotionEffect(keyPath: "center.x", type: .tiltAlongHorizontalAxis)
         horizontal.minimumRelativeValue = -amount
