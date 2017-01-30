cda0cff4391c16a48b14ba8b6ef42ea4c166b495
Yusuf <yusuf@eftsoftware.com>
2017-01-30 14:41:31 +0300

    Swift 2.3 added

--- Source/WobbleBubbleButton.swift
+++ Source/WobbleBubbleButton.swift
@@ -8,14 +8,14 @@
 
 import UIKit
 
-@IBDesignable
-open class WobbleBubbleButton: UIButton {
+
+public class WobbleBubbleButton: UIButton {
     
     public var bgImage:UIImage!
     
-    let screenSize: CGRect = UIScreen.main.bounds
+    let screenSize: CGRect = UIScreen.mainScreen().bounds
     
-    @IBInspectable var fittedBGImage: UIImage? {
+    public var fittedBGImage: UIImage? {
         
         didSet {
             var bFrame = self.frame
@@ -35,7 +35,7 @@ open class WobbleBubbleButton: UIButton {
             self.frame = bFrame
             
             let imView = UIImageView.init(frame: self.frame)
-            imView.contentMode = .scaleAspectFit
+            imView.contentMode = .ScaleAspectFit
             var imageViewFrame = imView.frame
             let heightRatio = self.frame.size.height / 1.82 // 1.82 = 155/85
             imageViewFrame.size.height = heightRatio
@@ -46,23 +46,26 @@ open class WobbleBubbleButton: UIButton {
             self.addSubview(imView)
             
             self.bgImage = self.fittedBGImage
+            var size = self.frame.size
+            self.layer.cornerRadius = size.width / 2
         }
     }
     
-    override open func awakeFromNib() {
+    override public func awakeFromNib() {
         self.addParallaxToView(self)
         getBaloonsFromUniverse()
         self.addAnimationForView(self)
     }
     
-    fileprivate func addParallaxToView(_ vw: UIView) {
+    
+    func addParallaxToView(_ vw: UIView) {
         let amount = 25
         
-        let horizontal = UIInterpolatingMotionEffect(keyPath: "center.x", type: .tiltAlongHorizontalAxis)
+        let horizontal = UIInterpolatingMotionEffect(keyPath: "center.x", type: .TiltAlongHorizontalAxis)
         horizontal.minimumRelativeValue = -amount
         horizontal.maximumRelativeValue = amount
         
-        let vertical = UIInterpolatingMotionEffect(keyPath: "center.y", type: .tiltAlongVerticalAxis)
+        let vertical = UIInterpolatingMotionEffect(keyPath: "center.y", type: .TiltAlongVerticalAxis)
         vertical.minimumRelativeValue = -amount
         vertical.maximumRelativeValue = amount
         
@@ -71,22 +74,24 @@ open class WobbleBubbleButton: UIButton {
         vw.addMotionEffect(group)
     }
     
-    fileprivate func random(min: Double, max: Double) -> Double {
+    
+    func random(min: Double, max: Double) -> Double {
         return Double(arc4random()) / 0xFFFFFFFF * (max - min) + min
     }
     
-    fileprivate func getBaloonsFromUniverse() {
+    func getBaloonsFromUniverse() {
         let instanceOrigin = self.frame.origin
         self.frame.origin = CGPoint.init(x: screenSize.width/2, y: screenSize.height + 400)
-        UIView.animate(withDuration: 2.0) {
+        UIView.animateWithDuration(2.0) {
             self.frame.origin = instanceOrigin
+            
         }
     }
     
     //
     //  Refering on http://stackoverflow.com/questions/23927047/button-animate-like-ios-game-center-button
     //
-    fileprivate func addAnimationForView(_ view: UIView) {
+    func addAnimationForView(_ view: UIView) {
         
         //create an animation to follow a circular path
         let pathAnimation = CAKeyframeAnimation(keyPath: "position")
@@ -95,49 +100,62 @@ open class WobbleBubbleButton: UIButton {
         pathAnimation.calculationMode = kCAAnimationPaced
         //apply transformation at the end of animation (not really needed since it runs forever)
         pathAnimation.fillMode = kCAFillModeForwards;
-        pathAnimation.isRemovedOnCompletion = false;
+        pathAnimation.removedOnCompletion = false;
         //run forever
         pathAnimation.repeatCount = Float.infinity;
         //no ease in/out to have the same speed along the path
         pathAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
-        pathAnimation.duration = random(min: 5, max: 8)
+        pathAnimation.duration = random(5, max: 8)
         
         
         //The circle to follow will be inside the circleContainer frame.
         //it should be a frame around the center of your view to animate.
         //do not make it to large, a width/height of 3-4 will be enough.
-        let curvedPath = CGMutablePath()
-//        path.addLine(to: CGPoint(x: 10.0, y: 10.0))
-
-//        let circleContainer = view.frame.insetBy(dx: 25/50 * view.frame.size.width, dy: 23/50 * view.frame.size.height)
+        let curvedPath = CGPathCreateMutable()
+        //        path.addLine(to: CGPoint(x: 10.0, y: 10.0))
         
-//        curvedPath.addEllipse(in: circleContainer)
+        //        let circleContainer = view.frame.insetBy(dx: 25/50 * view.frame.size.width, dy: 23/50 * view.frame.size.height)
+        
+        //        curvedPath.addEllipse(in: circleContainer)
         var startOrigin = self.frame.origin
         startOrigin.x = startOrigin.x + self.frame.size.width / 2
         startOrigin.y = startOrigin.y + self.frame.size.width / 2
         var topOrigin = startOrigin
-        topOrigin.x = topOrigin.x + CGFloat(random(min: 0, max: 20))
-        topOrigin.y = topOrigin.y + CGFloat(random(min: 0, max: 20))
+        topOrigin.x = topOrigin.x + CGFloat(random(0, max: 20))
+        topOrigin.y = topOrigin.y + CGFloat(random(0, max: 20))
         var rightOrigin = topOrigin
-        rightOrigin.x = rightOrigin.x + CGFloat(random(min: 0, max: 20))
-        rightOrigin.y = rightOrigin.y - CGFloat(random(min: 0, max: 20))
+        rightOrigin.x = rightOrigin.x + CGFloat(random(0, max: 20))
+        rightOrigin.y = rightOrigin.y - CGFloat(random(0, max: 20))
         var bottomOrigin = rightOrigin
-        bottomOrigin.x = bottomOrigin.x - CGFloat(random(min: 0, max: 20))
-        bottomOrigin.y = bottomOrigin.y + CGFloat(random(min: 0, max: 20))
+        bottomOrigin.x = bottomOrigin.x - CGFloat(random(0, max: 20))
+        bottomOrigin.y = bottomOrigin.y + CGFloat(random(0, max: 20))
         let endOrigin = startOrigin
-//        endOrigin.x = endOrigin.x + CGFloat(random(min: -10, max: 10))
-//        endOrigin.y = endOrigin.y + CGFloat(random(min: -10, max: 10))
-        curvedPath.addLines(between: [startOrigin,topOrigin,rightOrigin,bottomOrigin,endOrigin])
-        //add the path to the animation
+        //        endOrigin.x = endOrigin.x + CGFloat(random(min: -10, max: 10))
+        //        endOrigin.y = endOrigin.y + CGFloat(random(min: -10, max: 10))
+        //curvedPath.addLines(between: [startOrigin,topOrigin,rightOrigin,bottomOrigin,endOrigin])
+        //curvedPath.a
+        CGPathAddLines(curvedPath, nil, [startOrigin,topOrigin,rightOrigin,bottomOrigin,endOrigin], 5)
+        //TODO
+        
+        //add the path to the animdation
         pathAnimation.path = curvedPath;
         //release path
         //    CGPathRelease(curvedPath);
         //add animation to the view's layer
-        let delay = DispatchTime.now() + .seconds(10/4)
+        //let delay = DispatchTime.now() + .seconds(10/4)
+        //        dispatch_after(dispatch_time(DISPATCH_TIME_NOW,(2.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), {
+        //            view.layer.addAnimation(pathAnimation, forKey: "myCircleAnimation")
+        //            });
         
-        DispatchQueue.main.asyncAfter(deadline: delay) {
-            view.layer.add(pathAnimation, forKey: "myCircleAnimation")
+        let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(2 * Double(NSEC_PER_SEC)))
+        dispatch_after(delayTime, dispatch_get_main_queue()) {
+            view.layer.addAnimation(pathAnimation, forKey: "myCircleAnimation")
         }
+        
+        //        DispatchQueue.main.asyncAfter(deadline: delay) {
+        //            view.layer.add(pathAnimation, forKey: "myCircleAnimation")
+        //        }
+        
     }
     
     //
@@ -145,7 +163,7 @@ open class WobbleBubbleButton: UIButton {
     // Refering on https://developer.apple.com/library/mac/documentation/Cocoa/Reference/ApplicationKit/Classes/NSColor_Class/#//apple_ref/occ/instm/NSColor/blendedColorWithFraction:ofColor:
     // http://stackoverflow.com/questions/32293210/no-visible-interface-for-uicolor-declares-the-selector-blendedcolorwithfract
     //
-    override open func draw(_ rect: CGRect) {
+    func draw(_ rect: CGRect) {
         clipsToBounds = true
         layer.cornerRadius = frame.size.width/2
     }
@@ -153,17 +171,21 @@ open class WobbleBubbleButton: UIButton {
 
 extension UIView {
     func fadeIn(delay:Double? = nil) {
-        // Move our fade out code from earlier
-        UIView.animate(withDuration: 0.5, delay: delay != nil ? delay! : 0.0, options: UIViewAnimationOptions.curveEaseIn, animations: {
+        UIView.animateWithDuration(0.5, delay: delay!, options: .CurveEaseIn, animations: {
             self.alpha = 1.0
-            //            self.transform = self.transform.scaledBy(x: 2, y: 2)
+            
         }, completion: nil)
     }
     
-    func fadeOut(delay:Double? = nil) {
-        UIView.animate(withDuration: 0.5, delay: delay != nil ? delay! : 0.0, options: UIViewAnimationOptions.curveEaseOut, animations: {
-            self.alpha = 0.0
-            self.transform = self.transform.scaledBy(x: 0.5, y: 0.5)
-        }, completion: nil)
-    }
+    //    func fadeOut(delay:Double? = nil) {
+    //
+    //
+    //        UIView.animateWithDuration(0.5, delay: delay!, options: .CurveEaseOut, animations: {
+    //            self.alpha = 0.0
+    //            self.transform = self.transform.scaledBy(x: 0.5, y: 0.5)
+    //            //self.transform = self.transform.
+    //            self.tra
+    //            //TODO
+    //            }, completion: nil)
+    //    }
 }
