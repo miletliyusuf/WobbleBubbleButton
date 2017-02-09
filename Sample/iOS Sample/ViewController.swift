c0b07d2a69f181dfde336beb5ffcd9c065a9f8d2
Yusuf <yusuf@eftsoftware.com>
2017-02-09 14:47:55 +0300

    Migrated to swift 3

--- Sample/iOS Sample/ViewController.swift
+++ Sample/iOS Sample/ViewController.swift
@@ -7,41 +7,41 @@
 //
 
 import UIKit
-import WobbleBubbleButton
+//import WobbleBubbleButton
 
 class ViewController: UIViewController {
 
-  @IBOutlet weak var button1: WobbleBubbleButton!
-  @IBOutlet weak var button2: WobbleBubbleButton!
-  @IBOutlet weak var button3: WobbleBubbleButton!
+//  @IBOutlet weak var button1: WobbleBubbleButton!
+//  @IBOutlet weak var button2: WobbleBubbleButton!
+//  @IBOutlet weak var button3: WobbleBubbleButton!
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    configBubbleButton(button1, primaryText: "797", subText: "Points", backgroundColor: UIColor.green.withAlphaComponent(0.5))
-    configBubbleButton(button2, primaryText: "12", subText: "Friends", backgroundColor: UIColor.purple.withAlphaComponent(0.5))
-    configBubbleButton(button3, primaryText: "6", subText: "Games", backgroundColor: UIColor.cyan.withAlphaComponent(0.5))
+//    configBubbleButton(button1, primaryText: "797", subText: "Points", backgroundColor: UIColor.green.withAlphaComponent(0.5))
+//    configBubbleButton(button2, primaryText: "12", subText: "Friends", backgroundColor: UIColor.purple.withAlphaComponent(0.5))
+//    configBubbleButton(button3, primaryText: "6", subText: "Games", backgroundColor: UIColor.cyan.withAlphaComponent(0.5))
   }
   
-  func configBubbleButton(_ button: WobbleBubbleButton, primaryText: String, subText: String, backgroundColor: UIColor) {
-    button.backgroundColor = backgroundColor
-    button.titleLabel?.lineBreakMode = .byWordWrapping
-    button.titleLabel?.textAlignment = .center
-    let attributedText = NSAttributedString(string: primaryText + "\n",
-      attributes: [
-        NSForegroundColorAttributeName: UIColor.white,
-        NSFontAttributeName: UIFont.systemFont(ofSize: 40)
-      ])
-    let attributedDetailText = NSAttributedString(string: subText,
-      attributes: [
-        NSForegroundColorAttributeName: UIColor.white,
-        NSFontAttributeName: UIFont.systemFont(ofSize: 15)
-      ])
-    let attributedCombinedText = NSMutableAttributedString()
-    attributedCombinedText.append(attributedText)
-    attributedCombinedText.append(attributedDetailText)
-    button.setAttributedTitle(attributedCombinedText, for: UIControlState())
-  }
+//  func configBubbleButton(_ button: WobbleBubbleButton, primaryText: String, subText: String, backgroundColor: UIColor) {
+//    button.backgroundColor = backgroundColor
+//    button.titleLabel?.lineBreakMode = .byWordWrapping
+//    button.titleLabel?.textAlignment = .center
+//    let attributedText = NSAttributedString(string: primaryText + "\n",
+//      attributes: [
+//        NSForegroundColorAttributeName: UIColor.white,
+//        NSFontAttributeName: UIFont.systemFont(ofSize: 40)
+//      ])
+//    let attributedDetailText = NSAttributedString(string: subText,
+//      attributes: [
+//        NSForegroundColorAttributeName: UIColor.white,
+//        NSFontAttributeName: UIFont.systemFont(ofSize: 15)
+//      ])
+//    let attributedCombinedText = NSMutableAttributedString()
+//    attributedCombinedText.append(attributedText)
+//    attributedCombinedText.append(attributedDetailText)
+//    button.setAttributedTitle(attributedCombinedText, for: UIControlState())
+//  }
 
 
 }
--- Source/WobbleBubbleButton.swift
+++ Source/WobbleBubbleButton.swift
@@ -9,19 +9,19 @@
 import UIKit
 
 
-public class WobbleBubbleButton: UIButton {
+open class WobbleBubbleButton: UIButton {
     
-    public var bgImage:UIImage!
+    open var bgImage:UIImage!
     
-    let screenSize: CGRect = UIScreen.mainScreen().bounds
-    public var durationBubbleAppear: Double = 2.0
-    public var durationBubbleComeToView: Double = 2.0
-    public var imageViewFitBackground:UIImageView!
+    let screenSize: CGRect = UIScreen.main.bounds
+    open var durationBubbleAppear: Double = 2.0
+    open var durationBubbleComeToView: Double = 2.0
+    open var imageViewFitBackground:UIImageView!
     
-    public var fittedBGImage: UIImage? {
+    open var fittedBGImage: UIImage? {
         didSet {
             self.imageViewFitBackground = UIImageView.init(frame: self.frame)
-            self.imageViewFitBackground.contentMode = .ScaleAspectFit
+            self.imageViewFitBackground.contentMode = .scaleAspectFit
             var imageViewFrame = self.imageViewFitBackground.frame
             let heightRatio = self.frame.size.height / 1.82 // 1.82 = 155/85
             imageViewFrame.size.height = heightRatio
@@ -33,28 +33,28 @@ public class WobbleBubbleButton: UIButton {
         }
     }
     
-    override public func awakeFromNib() {
+    override open func awakeFromNib() {
         self.setAppearFrames()
         self.addParallaxToView(self)
     }
     
-    public func setTitleAndSubTitleText(title:String,subTitle:String,font:UIFont? = nil,subTitleFont:UIFont?=nil) {
-        self.titleLabel?.lineBreakMode = .ByWordWrapping
-        self.titleLabel?.textAlignment = .Center
+    open func setTitleAndSubTitleText(_ title:String,subTitle:String,font:UIFont? = nil,subTitleFont:UIFont?=nil) {
+        self.titleLabel?.lineBreakMode = .byWordWrapping
+        self.titleLabel?.textAlignment = .center
         let attributedText = NSAttributedString(string: title + "\n",
                                                 attributes: [
-                                                    NSForegroundColorAttributeName: subTitle == "" ? UIColor.whiteColor() : UIColor.init(red: 255/255.0, green: 255/255.0, blue: 255/255.0, alpha: 0.65),
-                                                    NSFontAttributeName: font != nil ? font! : UIFont.systemFontOfSize(24)
+                                                    NSForegroundColorAttributeName: subTitle == "" ? UIColor.white : UIColor.init(red: 255/255.0, green: 255/255.0, blue: 255/255.0, alpha: 0.65),
+                                                    NSFontAttributeName: font != nil ? font! : UIFont.systemFont(ofSize: 24)
             ])
         let attributedDetailText = NSAttributedString(string: "\n" + subTitle,
                                                       attributes: [
-                                                        NSForegroundColorAttributeName: UIColor.whiteColor(),
-                                                        NSFontAttributeName: subTitleFont != nil ? subTitleFont! : UIFont.systemFontOfSize(15)
+                                                        NSForegroundColorAttributeName: UIColor.white,
+                                                        NSFontAttributeName: subTitleFont != nil ? subTitleFont! : UIFont.systemFont(ofSize: 15)
             ])
         let attributedCombinedText = NSMutableAttributedString()
-        attributedCombinedText.appendAttributedString(attributedText)
-        attributedCombinedText.appendAttributedString(attributedDetailText)
-        self.setAttributedTitle(attributedCombinedText, forState: UIControlState.Normal)
+        attributedCombinedText.append(attributedText)
+        attributedCombinedText.append(attributedDetailText)
+        self.setAttributedTitle(attributedCombinedText, for: UIControlState())
     }
     
     func setAppearFrames() {
@@ -65,18 +65,18 @@ public class WobbleBubbleButton: UIButton {
         }
         
         self.bgImage = self.fittedBGImage
-        var size = self.frame.size
+        let size = self.frame.size
         self.layer.cornerRadius = size.width / 2
     }
     
     func addParallaxToView(_ vw: UIView) {
         let amount = 25
         
-        let horizontal = UIInterpolatingMotionEffect(keyPath: "center.x", type: .TiltAlongHorizontalAxis)
+        let horizontal = UIInterpolatingMotionEffect(keyPath: "center.x", type: .tiltAlongHorizontalAxis)
         horizontal.minimumRelativeValue = -amount
         horizontal.maximumRelativeValue = amount
         
-        let vertical = UIInterpolatingMotionEffect(keyPath: "center.y", type: .TiltAlongVerticalAxis)
+        let vertical = UIInterpolatingMotionEffect(keyPath: "center.y", type: .tiltAlongVerticalAxis)
         vertical.minimumRelativeValue = -amount
         vertical.maximumRelativeValue = amount
         
@@ -86,23 +86,20 @@ public class WobbleBubbleButton: UIButton {
     }
     
     
-    func random(min: Double, max: Double) -> Double {
+    func random(_ min: Double, max: Double) -> Double {
         return Double(arc4random()) / 0xFFFFFFFF * (max - min) + min
     }
     
-    public func getBaloonsFromUniverse() {
+    open func getBaloonsFromUniverse() {
         let instanceOrigin = self.frame.origin
         self.frame.origin = CGPoint.init(x: screenSize.width/2, y: screenSize.height + 400)
-        UIView.animateWithDuration(durationBubbleComeToView) {
+        UIView.animate(withDuration: durationBubbleComeToView, animations: {
             self.frame.origin = instanceOrigin
             
-        }
+        }) 
     }
-    
-    //
-    //  Refering on http://stackoverflow.com/questions/23927047/button-animate-like-ios-game-center-button
-    //
-    public func addAnimationForView(_ view: UIView) {
+
+    open func addAnimationForView(_ view: UIView) {
         
         //create an animation to follow a circular path
         let pathAnimation = CAKeyframeAnimation(keyPath: "position")
@@ -111,7 +108,7 @@ public class WobbleBubbleButton: UIButton {
         pathAnimation.calculationMode = kCAAnimationPaced
         //apply transformation at the end of animation (not really needed since it runs forever)
         pathAnimation.fillMode = kCAFillModeForwards;
-        pathAnimation.removedOnCompletion = false;
+        pathAnimation.isRemovedOnCompletion = false;
         //run forever
         pathAnimation.repeatCount = Float.infinity;
         //no ease in/out to have the same speed along the path
@@ -122,12 +119,8 @@ public class WobbleBubbleButton: UIButton {
         //The circle to follow will be inside the circleContainer frame.
         //it should be a frame around the center of your view to animate.
         //do not make it to large, a width/height of 3-4 will be enough.
-        let curvedPath = CGPathCreateMutable()
-        //        path.addLine(to: CGPoint(x: 10.0, y: 10.0))
-        
-        //        let circleContainer = view.frame.insetBy(dx: 25/50 * view.frame.size.width, dy: 23/50 * view.frame.size.height)
-        
-        //        curvedPath.addEllipse(in: circleContainer)
+        let curvedPath = CGMutablePath()
+
         var startOrigin = self.frame.origin
         startOrigin.x = startOrigin.x + self.frame.size.width / 2
         startOrigin.y = startOrigin.y + self.frame.size.width / 2
@@ -141,20 +134,16 @@ public class WobbleBubbleButton: UIButton {
         bottomOrigin.x = bottomOrigin.x - CGFloat(random(0, max: 20))
         bottomOrigin.y = bottomOrigin.y + CGFloat(random(0, max: 20))
         let endOrigin = startOrigin
-        //        endOrigin.x = endOrigin.x + CGFloat(random(min: -10, max: 10))
-        //        endOrigin.y = endOrigin.y + CGFloat(random(min: -10, max: 10))
-        //curvedPath.addLines(between: [startOrigin,topOrigin,rightOrigin,bottomOrigin,endOrigin])
-        //curvedPath.a
-        CGPathAddLines(curvedPath, nil, [startOrigin,topOrigin,rightOrigin,bottomOrigin,endOrigin], 5)
-        //TODO
+
+        curvedPath.addLines(between: [startOrigin,topOrigin,rightOrigin,bottomOrigin,endOrigin])
         
         //add the path to the animdation
         pathAnimation.path = curvedPath;
         
         let delay = durationBubbleAppear * Double(NSEC_PER_SEC)
-        let time = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
-        dispatch_after(time, dispatch_get_main_queue(), {
-            view.layer.addAnimation(pathAnimation, forKey: "myCircleAnimation")
+        let time = DispatchTime.now() + Double(Int64(delay)) / Double(NSEC_PER_SEC)
+        DispatchQueue.main.asyncAfter(deadline: time, execute: {
+            view.layer.add(pathAnimation, forKey: "myCircleAnimation")
         })
         
     }
@@ -164,15 +153,15 @@ public class WobbleBubbleButton: UIButton {
     // Refering on https://developer.apple.com/library/mac/documentation/Cocoa/Reference/ApplicationKit/Classes/NSColor_Class/#//apple_ref/occ/instm/NSColor/blendedColorWithFraction:ofColor:
     // http://stackoverflow.com/questions/32293210/no-visible-interface-for-uicolor-declares-the-selector-blendedcolorwithfract
     //
-    func draw(_ rect: CGRect) {
+    open override func draw(_ rect: CGRect) {
         clipsToBounds = true
         layer.cornerRadius = frame.size.width/2
     }
 }
 
 extension UIView {
-    func fadeIn(delay:Double? = nil) {
-        UIView.animateWithDuration(0.5, delay: delay!, options: .CurveEaseIn, animations: {
+    func fadeIn(_ delay:Double? = nil) {
+        UIView.animate(withDuration: 0.5, delay: delay!, options: .curveEaseIn, animations: {
             self.alpha = 1.0
             
         }, completion: nil)
